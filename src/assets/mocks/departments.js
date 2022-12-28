33e284a490da721ddab69b808cce58cd79068b1d
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-12-28 17:40:49 +0100

    Mocks init

---
+++ src/assets/mocks/departments.js
@@ -0,0 +1,22 @@
+export const DEPARTMENTS_MOCKS = [
+   {
+      value: 'sales',
+      label: 'Sales',
+   },
+   {
+      value: 'marketing',
+      label: 'Marketing',
+   },
+   {
+      value: 'engineering',
+      label: 'Engineering',
+   },
+   {
+      value: 'human resources',
+      label: 'Human Resources',
+   },
+   {
+      value: 'legal',
+      label: 'Legal',
+   },
+];

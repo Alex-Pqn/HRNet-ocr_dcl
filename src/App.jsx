f7529626eac379eb6b95b75a7e5758f78ca56623
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-12-28 17:40:18 +0100

    App & Router init

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './style/style.scss';
+
+import { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import DefaultHeader from './components/Default/DefaultHeader/DefaultHeader';
+import DefaultFooter from './components/Default/DefaultFooter/DefaultFooter';
+
+import EmployeeList from './pages/Employee/EmployeeList/EmployeeList';
+import Home from './pages/Home/Home';
+import Error404 from './pages/Error404/Error404';
+
+function App() {
+  return (
+    <div className="app">
+      <DefaultHeader />
+      <main>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="home" element={<Navigate to="/" />} />
+            <Route path="list" element={<EmployeeList />} />
+            <Route path="*" element={<Error404 />} />
+          </Route>
+        </Routes>
+      </main>
+      <DefaultFooter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Redux
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+// Root Reducer
+import rootReducer from './redux/reducers/index.js';
+
+// Router
+import { BrowserRouter } from 'react-router-dom';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+const persistore = persistStore(store);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <PersistGate persistor={persistore}>
+          <App />
+        </PersistGate>
+      </React.StrictMode>
+    </Provider>
+  </BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
